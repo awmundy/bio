5b46bfde4eb06d9c56d08d0d5a039bbe8182f630
Andrew Mundy <awmundy@gmail.com>
2022-01-01 17:32:51 -0500

    astqc working, started kallisto quant, other cleanupe

--- explore.py
+++ explore.py
@@ -3,17 +3,41 @@ import shutil
 from datetime import datetime as dt
 import subprocess
 
-def make_fastq_subdirs_and_move_fastq_files(fastq_root_dir):
+def rename_fastq_file(fastq_root_dir, fname):
+    suffix = fname.split('.fastq')[-1].split('.gz')[0]
+    pre_dot_fastq_fname = fname.split('.fastq')[0]
+    new_fname = pre_dot_fastq_fname + suffix + '.fastq.gz'
+
+    if fname != new_fname:
+        print('renaming', fname, 'to', new_fname)
+        os.rename(fastq_root_dir + fname, fastq_root_dir + new_fname)
+
+    return new_fname
+
+def rename_and_move_fastq_files(fastq_root_dir):
     files = [x for x in os.listdir(fastq_root_dir) if x.endswith('.gz')]
     if len(files) == 0:
         print('All fastq files already moved')
         return
 
-    for file in files:
-        subdir = fastq_root_dir + file.replace('.gz', '') + '/'
-        print('moving file', file, 'to', subdir)
+    for fname in files:
+        # for fastq files with weird extensions like .fastq-004.gz, move "-004" in to filename
+        new_fname = rename_fastq_file(fastq_root_dir, fname)
+
+        # store each fastq in its own subdir
+        subdir = fastq_root_dir + new_fname.replace('.fastq.gz', '') + '/'
+        print('moving file', new_fname, 'to', subdir)
         os.makedirs(subdir)
-        shutil.move(fastq_root_dir + file, subdir + file)
+        shutil.move(fastq_root_dir + new_fname, subdir + new_fname)
+
+
+def get_fastq_fpaths(fastq_dir):
+    """returns a list of all .fastq.gz files in the given fastq_dir's subdirectories"""
+    fastq_paths = []
+    for subdir in [x.path for x in os.scandir(fastq_dir) if x.is_dir()]:
+        fastq_paths += [x.path for x in os.scandir(subdir) if x.path.endswith('.fastq.gz')]
+
+    return fastq_paths
 
 
 def fastqc(fastq_dir, threads):
@@ -22,75 +46,90 @@ def fastqc(fastq_dir, threads):
     fastqc_dir = fastq_dir + '/fastqc/'
     if not os.path.exists(fastqc_dir):
         os.makedirs(fastqc_dir)
-    gz_paths = []
-    for subdir in [x.path for x in os.scandir(fastq_dir) if x.is_dir()]:
-        gz_paths += [x.path for x in os.scandir(subdir) if x.path.endswith('.gz')]
+
+    fastq_paths = get_fastq_fpaths(fastq_dir)
+
+    # dont overwrite if they already exist
+    for _path in fastq_paths:
+        fastqc_path = fastqc_dir + _path.split('/')[-1:][0].replace('.fastq.gz', '') + '_fastqc.html'
+        if os.path.exists(fastqc_path):
+            fastq_paths = [x for x in fastq_paths if x != _path]
 
 
-    cmd = 'fastqc ' + ' '.join(gz_paths)
-    cmd += f' -t {threads} -o {fastqc_dir}'
-    subprocess.run(cmd, shell=True)
+    if len(fastq_paths) > 0:
+        print('building fastqc output for the following fastq files:', fastq_paths)
+        cmd = 'fastqc ' + ' '.join(fastq_paths)
+        cmd += f' -t {threads} -o {fastqc_dir}'
+        subprocess.run(cmd, shell=True)
+    else:
+        print('All fastqc output already produced')
 
 
-def kallisto_build_index(fasta_filepath):
+def get_index_fpath_from_fasta_fpath(fasta_fpath):
+    index_fpath = fasta_fpath.replace('.fa.gz', '') + '.index'
+    return index_fpath
+
+
+def kallisto_build_index(fasta_fpath):
     # first step is to build an index on the reference fasta file.
     #  this will speed up allignment
-    index_filepath = fasta_filepath.replace('.fa.gz', '') + '.index'
+    index_fpath = get_index_fpath_from_fasta_fpath(fasta_fpath)
 
-
-    if os.path.exists(index_filepath):
-        print('index already exists for', fasta_filepath)
+    if os.path.exists(index_fpath):
+        print('index already exists for', fasta_fpath)
         return
 
+    subprocess.run(f'kallisto index -i {index_fpath} {fasta_fpath}',  shell=True)
 
-    # fasta_dir = os.path.dirname(fasta_filepath)
-    # fasta_dir = '/media/amundy/Windows/diyt/data/fasta/test/'
-    # fasta_file = 'Danio_rerio.GRCz11.cdna.all.fa.gz'
-    # fasta_filepath = fasta_dir + fasta_file
-    #
-    # index_file = fasta_file.replace('.fa.gz', '') + '.index'
-    # index_filepath = fasta_dir + index_file
-
-    subprocess.run(f'kallisto index -i {index_filepath} {fasta_filepath}',  shell=True)
+    return index_fpath
 
 
-def kallisto_quant():
-    # then we use the quant feature
-    # save in subdir labelled with the fastq label
-    output_dir = fastq_dir + fastq_label + '/'
+def kallisto_quant(index_fpath, fastq_dir, threads):
+
+    fastq_fpaths = get_fastq_fpaths(fastq_dir)[:2]
+    # todo remove
+    fastq_fpaths = fastq_fpaths[:2]
+    output_dir = fastq_dir + 'kallisto_quant' + '/'
+    os.makedirs(output_dir, exist_ok=True)
+
+    # todo add to args(probably a new arg that is a dict called kallisto_quant_args)
     # number of reads of the polymer that occurred during gene sequencing,
     #  pass --single for single read or empty string for paired (default execution is paired)
     read_end_type = '--single'
     # sequencing procedure has an average fragment length of the sequences (and standard deviation)
     frag_length = 250
     frag_length_sd = 30
-    log_file_path = fastq_filepath + '_' + dt.now().strftime('%Y_%m_%d_%Hh%Mm') + '.log'
-    quant_string = (f'kallisto quant '
-                    f'-i {index_filepath} '
-                    f'-o {output_dir} '
-                    f'-t {threads} '
-                    f'{read_end_type} '
-                    f'-l {frag_length} '
-                    f'-s {frag_length_sd} '
-                    f'{fastq_filepath} '
-                    f'> {log_file_path}'
-                    )
-    print(quant_string)
-    os.system(quant_string)
+
+    # todo consider not using a log this way and instead capturing stdout manually so that python console
+    #   shows it too
+    log_file_path = output_dir + 'log_' + dt.now().strftime('%Y_%m_%d_%Hh%Mm') + '.log'
+
+    cmd = (f'kallisto quant '
+           f'-i {index_fpath} '
+           f'-o {output_dir} '
+           f'-t {threads} '
+           f'{read_end_type} '
+           f'-l {frag_length} '
+           f'-s {frag_length_sd} '
+           )
+    cmd += + ' '.join(fastq_fpaths)
+    cmd += f'&> {log_file_path}'
+    print(cmd)
+    os.system(cmd)
 
 ## Flow ##
 # <Download fasta file> -> fasta file -> <kallisto> -> index
 # <Download fasta files> -> <Move fastq files> -> fastq files -> <fastqc> -> HTML output
 
 # http://ftp.ensembl.org/pub/release-105/fasta/homo_sapiens/cdna/
-fasta_filepath = '/media/amundy/Windows/diyt/data/fasta/Homo_sapiens.GRCh38.cdna.all.fa.gz'
-kallisto_build_index(fasta_filepath)
-
+fasta_fpath = '/media/amundy/Windows/diyt/data/fasta/Homo_sapiens.GRCh38.cdna.all.fa.gz'
 # files source (course dataset): https://drive.google.com/drive/folders/1sEk1od1MJKLjqyCExYyfHc0n7DAIy_x7
 fastq_dir = '/media/amundy/Windows/diyt/data/fastq/'
-make_fastq_subdirs_and_move_fastq_files(fastq_dir)
 
-fastqc(fastq_dir, threads=6)
+threads = 10
 
+index_fpath = kallisto_build_index(fasta_fpath)
+rename_and_move_fastq_files(fastq_dir)
+fastqc(fastq_dir, threads)
 
 
