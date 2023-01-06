462fd31016909169a0faeb12be52a2efacc2b0e0
awmundy <awmundy@gmail.com>
2023-01-06 12:59:18 -0500

    rename functions/dirs to be more accurate

--- download_geo_fastq_files.py
+++ download_geo_fastq_files.py
@@ -15,13 +15,13 @@ def write_to_log(output, log):
 
 
 
-def get_zhang_et_all_myofibers_sra_ids():
+def get_senescence_skeletal_muscle_myofiber_sra_ids():
     # paper: https://www.nature.com/articles/s43587-022-00250-8
 
     #geo: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE172254
 
     sra_ids = [
-        # 'SRR15931118',
+        'SRR15931118',
         'SRR15931119',
         'SRR15931120',
         'SRR15931121',
@@ -37,11 +37,11 @@ def get_zhang_et_all_myofibers_sra_ids():
 
     return sra_ids
 
-output_dir = '/media/awmundy/Windows/bio/senescence_skeletal_muscle/raw_rna_txs/'
-log_dir = '/media/awmundy/Windows/bio/senescence_skeletal_muscle/logs/'
+output_dir = '/media/awmundy/Windows/bio/senescence_skeletal_muscle_myofiber/rna_txs/'
+log_dir = '/media/awmundy/Windows/bio/senescence_skeletal_muscle_myofiber/logs/'
 os.makedirs(output_dir, exist_ok=True)
 os.makedirs(log_dir, exist_ok=True)
-sra_ids = get_zhang_et_all_myofibers_sra_ids()
+sra_ids = get_senescence_skeletal_muscle_myofiber_sra_ids()
 
 # requires sra-toolkit command line tool (apt install sra-toolkit)
 for sra_id in sra_ids:
